8d2975e46807a66fe8880cdca60414149aebc56a
Sambhav242005 <sambhav242005@gmail.com>
2025-11-08 13:17:18 +0530

    few thing

--- frontend/src/lib/api.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "http://localhost:5000/api", // your Flask backend
-  headers: { "Content-Type": "application/json" },
-});
-
-export default api;
